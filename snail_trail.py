241145b4b5dd51b4d370065191c76f9ca011f396
Kevin Diltinero <Kevindiltinero@gmail.com>
2019-03-08 09:54:30 +0000

    Add files via upload

---
+++ snail_trail.py
@@ -0,0 +1,82 @@
+        
+"""
+# -*- coding: utf-8 -*-
+Test.assert_equals(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]), "apples, pears\ngrapes\nbananas")
+Test.assert_equals(solution("a #b\nc\nd $e f g", ["#", "$"]), "a\nc\nd")
+"""
+
+print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
+
+import collections
+
+def snail(array):
+    ilen, jlen = len(array), len(array[0])
+    visited = set()
+    i, j = 0, 0   
+    
+    U = (i, j, ilen, jlen, visited)
+
+    while True:
+        print(U)
+        return False
+        R = right(*U)
+        if R == False:
+            return 
+        D = down(*R)
+        if D == False:
+            return         
+        L = left(*D)
+        if L == False:
+            return         
+        U = up(*L)
+        if U == False:
+            return  
+        # return False
+        
+def right(i, j, ilen, jlen, visited):    
+    print("Doing the first one")
+    if array[i][j+1] in visited:
+        return False
+    while j <= jlen-1:
+        if str(i)+str(j) not in visited:
+            print(array[i][j])
+            visited.add(str(i)+str(j))
+        j += 1
+    j -= 1
+    return i, j, ilen, jlen, visited
+    
+
+def down(i, j, ilen, jlen, visited):
+    print("At this point they are", i, j)
+    if array[i+1][j] in visited:
+        return False
+    while i <= ilen-1:
+        if str(i)+str(j) not in visited:  
+            # ''print(i, j)
+            print(array[i][j])
+            visited.add(str(i)+str(j))
+        i += 1
+    i -= 1
+    return i, j, ilen, jlen, visited
+        
+def left(i, j, ilen, jlen, visited):
+    if array[i][j-1] in visited:
+        return False
+    while j >= 0:
+        if str(i)+str(j) not in visited:  
+            # print(i, j)
+            print(array[i][j])
+            visited.add(str(i)+str(j))
+        j -= 1
+    j += 1
+    return i, j, ilen, jlen, visited
+        
+def up(i, j, ilen, jlen, visited):
+    if array[i-1][j] in visited:
+        return False
+    while i >= 0:
+        if str(i)+str(j) not in visited:  
+            # print(i, j)
+            print(array[i][j])
+            visited.add(str(i)+str(j))
+        i -= 1
