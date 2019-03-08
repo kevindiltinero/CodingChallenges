1ba48c427d062fdc5d4c13fdc64026a6de21eac7
Kevin Diltinero <Kevindiltinero@gmail.com>
2019-03-08 09:57:08 +0000

    Add files via upload

--- snail_trail.py
+++ snail_trail.py
@@ -80,3 +80,27 @@ def up(i, j, ilen, jlen, visited):
             print(array[i][j])
             visited.add(str(i)+str(j))
         i -= 1
+		
+
+
+
+"""
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+expected = [1,2,3,6,9,8,7,4,5]
+test.assert_equals(snail(array), expected)
+
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+expected = [1,2,3,4,5,6,7,8,9]
+test.assert_equals(snail(array), expected)
+"""
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+
+print(snail(array))
